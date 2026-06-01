327ca15814e9097c7d79239cceca3891faa343ed
tdawes-ch <tdawes@companieshouse.gov.uk>
2026-06-01 17:01:19 +0100

    created a test pipeline. Will expand on this later with db access

---
+++ src/pipelines/data_ingestion_test.py
@@ -0,0 +1,18 @@
+from processing.extraction import extract_all_races
+from scraping import season_scraper
+from database import init_db
+from database.management import connection as con
+from config.config import RAW_DATA_DIR, PROCESSED_DATA_DIR
+
+init_db.init_db()
+
+base_url = "https://www.formula1.com/en/results/{fyear}/races"
+start_year = 2023
+end_year = 2026
+
+season_scraper.download_years(base_url, start_year, end_year)
+
+for year in range(start_year,end_year+1):
+    html_path = str(RAW_DATA_DIR / year / f"{year}.html")
+    csv_path = str(PROCESSED_DATA_DIR / year / f"{year}.csv")
+    extract_all_races.extract_season_races(html_path,csv_path,base_url)

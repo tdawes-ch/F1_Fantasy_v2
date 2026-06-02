7e71b764f1b5420efdbc394e1426006e75f603c7
tdawes-ch <tomdawes3@gmail.com>
2026-06-03 00:02:34 +0100

    added file with description on what it's gonna do

---
+++ src/processing/extraction/extract_sessions.py
@@ -0,0 +1,20 @@
+"""
+This extracts sessions, given the race weekend url in:
+"""
+# data\sessions\raw\<year>\<year>.html
+"""
+Sessions:
+Practice 0 (called warm up in some years)
+Practice 1
+Practice 2 (non-sprint weekend)
+Practice 3 (non-sprint weekend)
+sprint-qualifying (sprint weekend)
+sprint-grid (sprint weekend)
+sprint (sprint weekend)
+qualifying
+starting-grid
+pit-stop-summary
+fastest-laps
+race-results
+
+"""

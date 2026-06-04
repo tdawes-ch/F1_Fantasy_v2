b495fe00fa3b9bf845a42af161d6919ef1bbbe6f
tdawes-ch <tomdawes3@gmail.com>
2026-06-04 23:05:23 +0100

    added bit to test connection before running

--- src/pipelines/data_ingestion_test.py
+++ src/pipelines/data_ingestion_test.py
@@ -1,4 +1,5 @@
 from processing.extraction import extract_all_races
+from toolbox import network
 from scraping import season_scraper
 from database import init_db
 from database.management import connection as con
@@ -10,9 +11,15 @@ base_url = "https://www.formula1.com/en/results/{fyear}/races"
 start_year = 2020
 end_year = 2026
 
-season_scraper.download_years(base_url, start_year, end_year)
+connection_ok, error = network.test_outbound_connection(base_url)
 
-for year in range(start_year,end_year+1):
-    html_path = RAW_DATA_DIR / str(year) / f"{year}.html"
-    csv_path = PROCESSED_DATA_DIR / str(year) / f"{year}.csv"
-    extract_all_races.extract_season_races(html_path,csv_path,base_url)
+if connection_ok:
+    season_scraper.download_years(base_url, start_year, end_year)
+
+    for year in range(start_year,end_year+1):
+        html_path = RAW_DATA_DIR / str(year) / f"{year}.html"
+        csv_path = PROCESSED_DATA_DIR / str(year) / f"{year}.csv"
+        extract_all_races.extract_season_races(html_path,csv_path,base_url)
+
+else:
+    print(error)

ad3065aa29a3184cb34954223704f5208b16b703
tdawes-ch <tomdawes3@gmail.com>
2026-06-04 23:04:46 +0100

    removed unimportant print test

--- src/toolbox/network.py
+++ src/toolbox/network.py
@@ -6,11 +6,9 @@ If that fails, there's something up with the network
 import requests
 from requests.exceptions import RequestException
 
-def test_outbound_connection(url: str = "https://www.google.com") -> list[bool, RequestException]:
+def test_outbound_connection(url: str = "https://www.google.com") -> tuple[bool, None | RequestException]:
     try:
         r = requests.get(url)
         return True, None
     except RequestException as e:
         return False, e
-
-print(test_outbound_connection()[0])

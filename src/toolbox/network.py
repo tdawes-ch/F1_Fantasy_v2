403b1cea1e750a1f1b215659089c6c4136aacf45
tdawes-ch <tdawes@companieshouse.gov.uk>
2026-06-04 17:42:03 +0100

    New file in toolbox to handle network tests/tasks. Current function just tests the connection with a default url of google. If it doesn't work, it returns an error. If it does work, it's just True

---
+++ src/toolbox/network.py
@@ -0,0 +1,16 @@
+"""
+does a ping to google.com.
+
+If that fails, there's something up with the network
+"""
+import requests
+from requests.exceptions import RequestException
+
+def test_outbound_connection(url: str = "https://www.google.com") -> list[bool, RequestException]:
+    try:
+        r = requests.get(url)
+        return True, None
+    except RequestException as e:
+        return False, e
+
+print(test_outbound_connection()[0])

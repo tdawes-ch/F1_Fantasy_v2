945e48df4ae1edc6a44a3be91c5a5a09693b96f6
Thomas Dawes <dawest1@cardiff.ac.uk>
2026-06-05 11:49:40 +0100

    added function to load csv into a list[dict]

--- src/toolbox/file_management.py
+++ src/toolbox/file_management.py
@@ -2,11 +2,11 @@ from bs4 import BeautifulSoup
 from pathlib import Path
 import csv
 
-def load_html_file(html_path: Path | str) -> BeautifulSoup:
-    html_path = Path(html_path)
+def load_html_file(filepath: Path | str) -> BeautifulSoup:
+    html_path = Path(filepath)
     # opens an html file as beautifulsoup
     if not html_path.exists():
-        raise FileNotFoundError(f"File not found: {path.name} in {path.parent}")
+        raise FileNotFoundError(f"File not found: {html_path.name} in {html_path.parent}")
 
     if html_path.suffix.lower() != ".html":
         raise FileNotFoundError("File must be a .html file")
@@ -28,3 +28,14 @@ def write_to_csv(data: list[dict], csv_path: Path | str, headers: list[str]):
         writer = csv.DictWriter(f, fieldnames=headers)
         writer.writeheader()
         writer.writerows(data)
+
+def load_csv(csv_path: Path | str) -> list[dict]:
+    csv_path = Path(csv_path)
+    results = []
+    with open(csv_path, "r", newline="", encoding="utf-8") as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            results.append(row)
+    return results
+
+def get

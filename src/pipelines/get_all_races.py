015748399ecaec98bcb8e0c8eed1a131e07c245c
tdawes-ch <tomdawes3@gmail.com>
2026-06-12 17:13:47 +0100

    added progress bar

--- src/pipelines/get_all_races.py
+++ src/pipelines/get_all_races.py
@@ -5,6 +5,7 @@ from database import init_db
 from database.management import connection as con
 from config.config import RAW_DATA_DIR, PROCESSED_DATA_DIR
 from pathlib import Path
+from rich.progress import Progress
 
 def _create_csv_path(dir: Path, year: int) -> Path:
     return dir / str(year) / f"{year}.csv"
@@ -12,13 +13,20 @@ def _create_csv_path(dir: Path, year: int) -> Path:
 def _create_html_path(dir: Path, year: int) -> Path:
     return dir / str(year) / f"{year}.html"
 
-def get_all_races(start_year: int, end_year: int, base_url):
-    # this is network dependent, compartmentalises it so I can test it 
-    # without scraping EVERYTHING and wasting bandwidth
-    season_scraper.download_years(base_url, start_year, end_year)
+def run(start_year: int, end_year: int, base_url, progress: Progress):
+    num_seasons = end_year + 1 - start_year
+    # scrape the years
+    season_scraper.download_years(progress, base_url, start_year, end_year)
 
+    # setup progress bar
+    extraction_task = progress.add_task(f"Processing season: [bold magenta]{start_year}[/bold magenta]", total=num_seasons)
+    # extract all race names and stuff from the html
     for year in range(start_year,end_year+1):
+        progress.update(extraction_task, description=f"Processing season: [bold magenta]{year}[/bold magenta]")
         html_path = _create_html_path(dir=RAW_DATA_DIR, year=year)
         csv_path = _create_csv_path(dir=PROCESSED_DATA_DIR, year=year)
-        extract_all_races.extract_season_races(html_path,csv_path,base_url) # gets all
+        extract_all_races.extract_season_races(html_path,csv_path,base_url) # gets all races
         # At this point, we've got season data and basic race data in the scraped tables. We now need more indepth race info
+        progress.advance(extraction_task)
+
+    progress.update(extraction_task, description=f"[green]✓ Race information processed[/green]")

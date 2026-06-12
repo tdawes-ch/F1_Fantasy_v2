87fcceeef8f15db7ca7a9f40683c7b3626944419
tdawes-ch <tomdawes3@gmail.com>
2026-06-12 17:22:16 +0100

    finally wrote from_db() to get the race ID from the database

--- src/toolbox/extract_race_id.py
+++ src/toolbox/extract_race_id.py
@@ -1,5 +1,7 @@
 from pathlib import PurePosixPath
 from urllib.parse import urlparse
+from database.management import connection
+from config.config import DB_PATH
 
 """ 
 this gets the race id from the url e.g.:
@@ -20,6 +22,35 @@ def from_url(url: str) -> int | None:
         race_id = None # None is essentially NULL
         return race_id
     
-def from_db():
-    # maybe a tool needed to extract a race id given some parameters. Will use SQL
-    pass
+def from_db(data: str | list[int], flag:str) -> int:
+    valid_flag = ("year,round", "url")
+    flag = flag.lower().strip()
+
+    if flag not in valid_flag:
+        raise ValueError(f"Incorrect flag passed into extract_race_id.from_db(): {flag}")
+    elif flag == valid_flag[0] and isinstance(data, list): ## year,round
+        if len(data) != 2:
+            raise ValueError(f"Incorrect list passed for round,year extract_race_id.from_db(): {data}. Expected [int,int]")
+        else:
+            with connection.get_db(DB_PATH) as conn:  # type: ignore
+                cursor = conn.cursor()
+                cursor.execute("""SELECT race_id
+                                    FROM scrape_race_weekends
+                                WHERE year = ? 
+                                  AND round = ?;
+                                """,(data[0], data[1])
+                                    )
+                race_id = cursor.fetchone()
+            return int(race_id[0])
+    elif flag == "url":
+        with connection.get_db(DB_PATH) as conn:  # type: ignore
+            cursor = conn.cursor()
+            cursor.execute("""SELECT race_id
+                                FROM scrape_race_weekends
+                            WHERE url = ? ;
+                            """,(data, )
+                                )
+            race_id = cursor.fetchone()
+        return int(race_id[0])
+    else:
+        raise ValueError("Unknown error occurred in extract_race_id.from_db() for values:/n - data: {data} /n - flag: {flag}")

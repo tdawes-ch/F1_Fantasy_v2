9907f0afbdc4d732fcc120133e38a13b132f6c43
tdawes-ch <tomdawes3@gmail.com>
2026-06-12 17:22:30 +0100

    added function to chekc if a location/file exists

--- src/toolbox/file_management.py
+++ src/toolbox/file_management.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 from pathlib import Path
 import csv
+from typing import Literal
 
 def load_html_file(filepath: Path | str) -> BeautifulSoup:
     html_path = Path(filepath)
@@ -30,6 +31,7 @@ def write_to_csv(data: list[dict], csv_path: Path | str, headers: list[str]):
         writer.writerows(data)
 
 def load_csv(csv_path: Path | str) -> list[dict]:
+    # loads a csv into a list of dictionaries where the dictionary is in the format {header: data}
     csv_path = Path(csv_path)
     results = []
     with open(csv_path, "r", newline="", encoding="utf-8") as f:
@@ -39,7 +41,17 @@ def load_csv(csv_path: Path | str) -> list[dict]:
     return results
 
 def create_path(dir_path: Path):
+    # creates filepath at location
     try:
         dir_path.mkdir(parents=True, exist_ok=True)
     except OSError as e:
         raise OSError(f"Failed to create directory at {dir_path}. Original error: {e}")
+    
+def check_location(filepath: Path, flag: Literal['dir', 'file'] = 'dir') -> bool:
+    # Returns True if the filepath exists, False otherwise.
+    if flag.lower() == 'dir':
+        return Path(filepath).exists()
+    elif flag.lower() == 'file':
+        return Path(filepath).is_file()
+    else:
+        raise ValueError(f"Incorrect flag passed to check_path: {flag}")

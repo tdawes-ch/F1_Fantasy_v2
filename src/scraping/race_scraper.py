b453b0a27dd5e3a81a9b4891d32fa3cc72408977
tdawes-ch <tomdawes3@gmail.com>
2026-06-12 17:21:11 +0100

    function now gets urls and scrapes them

--- src/scraping/race_scraper.py
+++ src/scraping/race_scraper.py
@@ -6,32 +6,21 @@ download_races gets called, with a list of urls and the season year as the param
 1. 
 """
 
-from pathlib import Path, PurePosixPath
-import datetime
+from pathlib import Path
 from scraping.bones import html_scraper_toolbox as scraper
-from config.config import RAW_DATA_DIR, LOG_DATA_DIR, DB_PATH
-import toolbox.file_management as fm
-import toolbox.extract_race_id as get_id
+from config.config import RAW_DATA_DIR, DB_PATH
+from toolbox import extract_race_id, database_query
 from database.management import connection
-from rich.console import Console
-from rich.live import Live
-from rich.console import Group
-from rich.text import Text
-from rich.progress import (
-    Progress, 
-    BarColumn, 
-    TaskProgressColumn, 
-    TimeRemainingColumn, 
-    MofNCompleteColumn
-)
+from rich.progress import Progress
+import datetime
 
 
-def create_path(year: int, url: str, race_name: str) -> Path:
+def _create_path(year: int, url: str, race_name: str) -> Path:
     # takes url, turns it into /2020/<race_name>
     path = RAW_DATA_DIR /  str(year) / race_name / "race_results.html"
     return path
 
-def get_race_name(url: str):
+def _get_race_name(url: str):
     with connection.get_db(DB_PATH) as conn:  # type: ignore
         cursor = conn.cursor()
         cursor.execute("""SELECT race_name
@@ -42,20 +31,70 @@ def get_race_name(url: str):
         race_name = cursor.fetchone()
     return race_name[0]
 
-def write_to_db(url: str):
+def _write_to_scrape_weekends(url: str, output_path: Path):
     # writes the scraper update to the database
     with connection.get_db(DB_PATH) as conn:  # type: ignore
         cursor = conn.cursor()
         cursor.execute("""UPDATE scrape_race_weekends
-                             SET scraped = 1
+                             SET scraped = 1, filepath = ?, last_scraped = ?
                            WHERE url = ?;
-                        """,(url, )
+                        """,(str(output_path), 
+                             database_query.get_last_scraped(url),
+                             url)
                             )
         # could also insert to scrape_sessions for race results, but will do that elsewhere
+
+def _write_to_scrape_seasons(year:int):
+    with connection.get_db(DB_PATH) as conn:  # type: ignore
+        cursor = conn.cursor()
+        cursor.execute("""UPDATE scrape_seasons
+                             SET has_races = 1
+                           WHERE year = ?;
+                        """,(str(year),)
+        )
+
+def _write_to_scrape_sessions(url: str, year:int, filepath: Path):
+    race_id = extract_race_id.from_db(data=url, flag="url")
+    with connection.get_db(DB_PATH) as conn:  # type: ignore
+        cursor = conn.cursor()
+        cursor.execute("""
+                        INSERT INTO scrape_sessions (race_id, year, session_type, url, filepath, scraped, last_scraped)
+                        VALUES (?, ?, ?, ?, ?, ?, ?)
+                        ON CONFLICT(url) DO UPDATE SET
+                            last_scraped = EXCLUDED.last_scraped;
+                        """,
+                        (race_id, 
+                         year,
+                         "Race Results",
+                         url,
+                         str(filepath),
+                         1,
+                         database_query.get_last_scraped(url)
+                            )
+                        )
     
-def download_races(urls: list[str], year: int):
-    for url in urls:
-        race_name = get_race_name(url)
-        output_path = create_path(year, url, race_name)
-        scraper.html_scraper(url, output_path)
-        write_to_db(url)
+def download_races(urls: list[str], year: int, progress: Progress):
+    ## progresss bar
+    download_task = progress.add_task(f"  ↳ Downloading URL: ", total=len(urls))
+
+    if urls:
+        _write_to_scrape_seasons(year) # sets has_races to 1
+        for url in urls:
+            short_url = f"f1.com/.../{url.split('/races/')[-1]}"
+            task_message = f"{short_url:<40}"
+            progress.update(download_task, description=f"  ↳ Downloading URL: [grey11]{task_message}[/grey11]")
+
+            race_name = _get_race_name(url)
+            output_path = _create_path(year, url, race_name)
+            scraper.html_scraper(url, output_path)
+
+            task_message = f"{"Writing to database...":<40}"
+            progress.update(download_task, description=f"  ↳ Downloading URL: [grey11]{task_message}[/grey11]")
+            _write_to_scrape_weekends(url, output_path)
+            _write_to_scrape_sessions(url, year, output_path)
+
+            progress.advance(download_task)
+            
+        progress.remove_task(download_task)
+    else:
+        return

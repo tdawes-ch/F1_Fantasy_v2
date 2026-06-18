7a08838c956018a5687f6f7e43aa69b7ef0e44fc
tdawes-ch <tomdawes3@gmail.com>
2026-06-18 09:16:28 +0100

    placemarker file for an offline mode pipeline?

--- src/pipelines/offline_mode.py
+++ src/pipelines/offline_mode.py
@@ -1,1 +1,6 @@
 # separate file for offline functions?
+
+## first check the database. If there's no data, check for html or csv files?
+
+## if there's literally nothing, then we should quit
+## if there's sufficient data, we can do some processing, but idk what yet

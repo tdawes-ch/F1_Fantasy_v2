97f58ca728af88090136860fc974bbd4590216ca
tdawes-ch <tomdawes3@gmail.com>
2026-06-18 23:21:27 +0100

    changed sql script

--- testing/code/database/test_sql_select.py
+++ testing/code/database/test_sql_select.py
@@ -3,10 +3,14 @@ from database.management import connection
 
 with connection.get_db(DB_PATH) as conn:  # type: ignore
         cursor = conn.cursor()
-        cursor.execute("""SELECT DISTINCT(race_name)
-                            FROM scrape_race_weekends
-                           WHERE race_id = 1045;
-                        """)
+        cursor.execute("""
+                        SELECT srw.race_name
+                          FROM scrape_race_weekends srw
+                          LEFT JOIN scrape_sessions ss ON srw.race_id = ss.race_id
+                         WHERE ss.url = ?;
+                           """,
+                           ("https://www.formula1.com/en/results/1999/races/687/australia/practice/2",)
+                           )
         output = cursor.fetchall()
  
 for row in output:

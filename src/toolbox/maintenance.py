3cccce34df126c2d4f932b60ac7662616be0c39f
Thomas Dawes <dawest1@cardiff.ac.uk>
2026-06-22 12:03:35 +0100

    placeholder file created for maintenance tasks

---
+++ src/toolbox/maintenance.py
@@ -0,0 +1,6 @@
+# need a function that goes through all urls in the tables, 
+# checks their location, and updates whether it was scraped or not accordingly
+
+# another function that deletes these unscraped records
+
+# another function that drops and recreates the database

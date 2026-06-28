52e79a99f67425f4682d9e64222dcba9fa91c534
Thomas Dawes <dawest1@cardiff.ac.uk>
2026-06-28 19:33:24 +0100

    added temp code

--- src/processing/cleaning/results_cleaner.py
+++ src/processing/cleaning/results_cleaner.py
@@ -1,5 +1,13 @@
 '''
 this takes in a results table (from extract_results) and will do the following:
 
-
+- Create driver info and populate appropriate tables
+- Create constructor information and populate appropriate tables
+- Populate results
 '''
+from pprint import pp
+
+
+
+def run(results):
+    pp(results)

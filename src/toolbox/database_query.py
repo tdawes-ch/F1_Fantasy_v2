d8a02130324910f25e794ccd320c02e9cb4ad1b3
Thomas Dawes <dawest1@cardiff.ac.uk>
2026-06-28 19:34:26 +0100

    added functions to get a list of race ids from a year and a second function to get a race name from a race ID

--- src/toolbox/database_query.py
+++ src/toolbox/database_query.py
@@ -40,3 +40,55 @@ def get_scraped_races(year: int) -> int:
                             )
         scraped_races = cursor.fetchone()
     return int(scraped_races[0])
+
+def get_race_ids(year: int, has_sessions: bool = False) -> list[int]:
+    """Gets a list of race IDs for a given year.
+
+    Args:
+        year (int): Year of F1 season
+        has_sessions (bool, optional): Used to choose whether to get race IDs only for weekends that have sessions or not. Defaults to False.
+
+    Returns:
+        list[int]: List of race IDs stored as integers
+    """    
+    race_ids = []
+    with connection.get_db(DB_PATH) as conn:  # type: ignore
+        cursor = conn.cursor()
+        if has_sessions:
+            cursor.execute("""
+                           SELECT race_id
+                             FROM scrape_race_weekends
+                            WHERE year = ?
+                              AND has_sessions = ?;
+                            """,(year,int(has_sessions))
+                                )
+        else:
+            cursor.execute("""
+                           SELECT race_id
+                             FROM scrape_race_weekends
+                            WHERE year = ?;
+                            """,(year,)
+                                )
+        output = cursor.fetchall()
+    for row in output:
+        race_ids.append(row[0])
+    return race_ids
+
+def get_race_name(race_id: int) -> str:
+    """Gets the name of a race from a race ID
+
+    Args:
+        race_id (int): The race ID
+
+    Returns:
+        str: The race name
+    """    
+    with connection.get_db(DB_PATH) as conn:  # type: ignore
+        cursor = conn.cursor()
+        cursor.execute("""SELECT race_name
+                            FROM scrape_race_weekends
+                        WHERE race_id = ?;
+                        """,(race_id,)
+                            )
+        race_name = cursor.fetchone()[0]
+    return race_name

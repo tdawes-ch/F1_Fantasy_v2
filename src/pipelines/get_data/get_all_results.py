bb62815bd5311c1c5426a281d012c405033e1c79
tdawes-ch <tomdawes3@gmail.com>
2026-06-28 22:59:23 +0100

    added progress bar commands changed session_type to session_name added progress to the test function

--- src/pipelines/get_data/get_all_results.py
+++ src/pipelines/get_data/get_all_results.py
@@ -6,6 +6,7 @@ from bs4 import BeautifulSoup
 from toolbox import file_management as fm
 from toolbox import database_query
 from database.management import connection
+from interface.progress_manager import get_progress_bar
 from pprint import pprint
 from config.config import DB_PATH, PROCESSED_DATA_DIR
 from rich.progress import Progress
@@ -36,38 +37,51 @@ def _get_session_info(race_id: int) -> list[dict]:
     
     for row in output:
         sessions.append({
-            "session_type":row[0].strip(),
+            "session_name":row[0].strip(),
             "url":row[1].strip(),
             "filepath":row[2].strip()
         })
     
     return sessions
 
-def _make_output_path(year, race_name, session_type) -> Path:
-    session = "_".join(session_name for session_name in session_type.lower().split(" "))
+def _make_output_path(year, race_name, session_name) -> Path:
+    session = "_".join(name for name in session_name.lower().split(" "))
     return PROCESSED_DATA_DIR / str(year) / race_name / f"{session}.csv"
 
-def run(start_year: int, end_year: int):
+def run(start_year: int, end_year: int, progress: Progress):
     # setup progress bar
     num_seasons = end_year + 1 - start_year
-    #year_task = progress.add_task(f"[bold magenta]{start_year}:[/bold magenta]", total=num_seasons)
+    year_task = progress.add_task(f"[bold magenta]Current year: {start_year}[/bold magenta]", total=num_seasons)
 
     for year in range(start_year, end_year+1):
+        progress.update(year_task, description=f"[bold magenta]Current year: {year}[/bold magenta]")
+
         # get race_ids
         race_ids = database_query.get_race_ids(year=year, has_sessions=True)
+        race_task = progress.add_task(f"└ Getting sessions", total=len(race_ids))
+
         for race_id in race_ids:
+            race_name = database_query.get_race_name(race_id)
+            progress.update(race_task, description=f"└ Getting sessions for [b]{race_name}[b]")
             session_data = _get_session_info(race_id=race_id)
+            
             for session in session_data:
+                progress.update(race_task, description=f"└ Processing results for [b]{race_name}: [dim]{session["session_name"]}[/dim][b]")
                 session_html = fm.load_html_file(filepath=session["filepath"])
                 session_results = extract_results.get_results(soup=session_html)
-                race_name = database_query.get_race_name(race_id)
-                output_path = _make_output_path(year, race_name, session["session_type"])
+                
+                output_path = _make_output_path(year, race_name, session["session_name"])
                 fm.write_to_csv(data=session_results,
                                 csv_path=output_path,
                                 headers=fm.get_headers(session_results))
-                results_cleaner.run(session_results)
+                results_cleaner.run(year,
+                                    race_id,
+                                    session_name=session["session_name"],
+                                    results=session_results,
+                                    url=session["url"])
                 
 def test():
-    run(2026,2026)
+    with get_progress_bar() as migration_progress:
+        run(2026,2026,migration_progress)
 
-test()
+#test()

e8b08963627a1f176e548b8c4b068b95a56bee11
tdawes-ch <tomdawes3@gmail.com>
2026-06-28 23:02:15 +0100

    added function to add driver details to database added function to add driver number and code to database added function to do all driver processing scripts in one go and added that to the main run() function

--- src/processing/cleaning/results_cleaner.py
+++ src/processing/cleaning/results_cleaner.py
@@ -6,8 +6,82 @@ this takes in a results table (from extract_results) and will do the following:
 - Populate results
 '''
 from pprint import pp
+from toolbox import file_management as fm
+from config.config import DB_PATH
+from database.management import connection
 
+def _insert_driver_details(driver_id: str, fname: str, lname: str):
+    with connection.get_db(DB_PATH) as conn:  # type: ignore
+        cursor = conn.cursor()
+        cursor.execute("""
+                        INSERT INTO race_drivers (driver_id,
+                                                  forename,
+                                                  surname
+                                                 )
+                        VALUES (?, ?, ?)
+                        ON CONFLICT (driver_id) DO NOTHING;
+                        """,
+                        (driver_id,
+                         fname,
+                         lname
+                         )
+                        )
 
+def _insert_driver_number(driver_id: str, driver_number: int, year: int):
+    with connection.get_db(DB_PATH) as conn:  # type: ignore
+        cursor = conn.cursor()
+        cursor.execute("""
+                        INSERT INTO race_driver_number_history (driver_id,
+                                                                number,
+                                                                season
+                                                                )
+                        VALUES (?, ?, ?)
+                        ON CONFLICT (driver_id, season) DO NOTHING;
+                        """,
+                        (driver_id,
+                         driver_number,
+                         year
+                         )
+                        )
 
-def run(results):
-    pp(results)
+def _insert_driver_code(driver_id: str, driver_code: int, year: int):
+    with connection.get_db(DB_PATH) as conn:  # type: ignore
+        cursor = conn.cursor()
+        cursor.execute("""
+                        INSERT INTO race_driver_code_history (driver_id,
+                                                              code,
+                                                              season
+                                                             )
+                        VALUES (?, ?, ?)
+                        ON CONFLICT (driver_id, season) DO NOTHING;
+                        """,
+                        (driver_id,
+                         driver_code,
+                         year
+                         )
+                        )
+
+def _driver_processing(results: list[dict], year: int):
+    headers = fm.get_headers(results)
+    required = {"Driver","Team","No."}
+
+    if not required.issubset(set(headers)): # checks if there are the appropriate driver headers
+        raise ValueError(f"Expected driver detail headings not found ({required})")
+
+    for result in results:
+        driver, team, number = result["Driver"], result["Team"], result["No."]
+        if len(driver) != 3:
+            raise ValueError(f"Expected three items under driver (fname, lname, code), got {len(driver)}: {driver}")
+        driver_id = f"{driver[0]}_{driver[1]}".lower()
+        _insert_driver_details(driver_id, fname=driver[0], lname=driver[1])
+        _insert_driver_number(driver_id, number, year)
+        _insert_driver_code(driver_id, driver_code=driver[2], year=year)
+        
+
+def run(year: int, 
+        race_id: int, 
+        session_name: str, 
+        results: list[dict], 
+        url: str): 
+    #pp(results)
+    _driver_processing(results,year)

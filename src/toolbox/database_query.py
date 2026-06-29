e37db20233d79f917d45fa09ec62996fef83f5a2
tdawes-ch <tomdawes3@gmail.com>
2026-06-29 19:04:30 +0100

    new functions to: - get session ID from url - get current number of qualy sessions from url improved doctype notation added error handling in case nothing is returned from the database. need to add this to lots of bits of code lol

--- src/toolbox/database_query.py
+++ src/toolbox/database_query.py
@@ -2,14 +2,14 @@ from config.config import DB_PATH
 from database.management import connection
 from datetime import datetime
 
-def get_last_scraped(url: str) -> datetime:
+def get_last_scraped(url: str) -> datetime | None:
     """Gets the most recent timestamp for when a specific URL was scraped using the scrape_log table
 
     Args:
         url (str): The URL to be checked
 
     Returns:
-        datetime: The most recent datetime 
+        datetime | None: The most recent datetime. None if not found
     """    
     with connection.get_db(DB_PATH) as conn:  # type: ignore
         cursor = conn.cursor()
@@ -19,17 +19,21 @@ def get_last_scraped(url: str) -> datetime:
                            ORDER BY id DESC;
                         """,(url,)
                             )
-        last_scraped = cursor.fetchone()
-    return last_scraped[0]
+        output = cursor.fetchone()
+        if output:
+            last_scraped = output[0]
+        else:
+            last_scraped = None
+    return last_scraped
 
-def get_scraped_races(year: int) -> int:
+def get_scraped_races(year: int) -> int | None:
     """Gets the number that sits in the scraped_races column for a specific year
 
     Args:
         year (int): The F1 season year
 
     Returns:
-        int: The number that sits in the scraped_races column for that year
+        int | None: The number that sits in the scraped_races column for that year. "None" if none
     """    
     with connection.get_db(DB_PATH) as conn:  # type: ignore
         cursor = conn.cursor()
@@ -38,8 +42,12 @@ def get_scraped_races(year: int) -> int:
                            WHERE year = ?;
                         """,(year,)
                             )
-        scraped_races = cursor.fetchone()
-    return int(scraped_races[0])
+        output = cursor.fetchone()
+        if output:
+            scraped_races = int(output[0])
+        else: 
+            scraped_races = None
+    return scraped_races
 
 def get_race_ids(year: int, has_sessions: bool = False) -> list[int]:
     """Gets a list of race IDs for a given year.
@@ -74,14 +82,14 @@ def get_race_ids(year: int, has_sessions: bool = False) -> list[int]:
         race_ids.append(row[0])
     return race_ids
 
-def get_race_name(race_id: int) -> str:
+def get_race_name(race_id: int) -> str | None:
     """Gets the name of a race from a race ID
 
     Args:
         race_id (int): The race ID
 
     Returns:
-        str: The race name
+        str | None: The race name, "None" if not found.
     """    
     with connection.get_db(DB_PATH) as conn:  # type: ignore
         cursor = conn.cursor()
@@ -90,5 +98,53 @@ def get_race_name(race_id: int) -> str:
                         WHERE race_id = ?;
                         """,(race_id,)
                             )
-        race_name = cursor.fetchone()[0]
+        output = cursor.fetchone()
+        if output:
+            race_name = output[0]
+        else:
+            race_name = None
     return race_name
+
+def get_session_id(url: str) -> int:
+    """Gets the name of a session from a URL
+
+    Args:
+        url (str): The URL of the session
+
+    Returns:
+        int: The session ID
+    """    
+    with connection.get_db(DB_PATH) as conn:  # type: ignore
+        cursor = conn.cursor()
+        cursor.execute("""SELECT session_id
+                            FROM scrape_sessions
+                           WHERE url = ?;
+                        """,(url,)
+                            )
+        session_id = int(cursor.fetchone()[0])
+    return session_id
+
+def get_n_qualy_sessions(session_id: int) -> int | None:
+    """Gets the current number of qualifying rounds for a set session_id
+
+    Args:
+        session_id (int): The session ID
+
+    Returns:
+        int | None: Number of qualifying rounds, "None" if not found.
+    """    
+    with connection.get_db(DB_PATH) as conn:  # type: ignore
+        cursor = conn.cursor()
+        cursor.execute("""SELECT qualifying_round
+                            FROM qualifying_times
+                           WHERE session_id = ?
+                           ORDER BY qualifying_round DESC;
+                        """,(session_id,)
+                            )
+        output = cursor.fetchone()
+
+        if output:
+            qualy_session = int(output[0])
+        else:
+            qualy_session = None
+    return qualy_session

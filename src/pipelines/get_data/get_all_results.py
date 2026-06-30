047d2072c0c195db62c3f3a5db228571cf1a9ae1
tdawes-ch <tomdawes3@gmail.com>
2026-06-30 23:09:25 +0100

    added "_update_scrape_sessions_table" to update the has_data column. This is used after race results are processed to show that the session has data and has been processed
    Added type comments to "_make_output_path" and added docstring notation
    
    Made it so that the progress bar updates properly too after each year has been processed

--- src/pipelines/get_data/get_all_results.py
+++ src/pipelines/get_data/get_all_results.py
@@ -25,6 +25,17 @@ We need to go through each year from start to end,
             - Write results to database
 """
 def _get_session_info(race_id: int) -> list[dict]:
+    """Internal function to get session information from the database
+
+    Args:
+        race_id (int): _description_
+
+    Returns:
+        list[dict]: The session information in the form: 
+                    {"session_name": (name of the session)
+                     "url": (url of the session results page)
+                     "filepath": (filepath to the saved HTML)}
+    """    
     sessions = []
     with connection.get_db(DB_PATH) as conn:  # type: ignore
         cursor = conn.cursor()
@@ -44,44 +55,76 @@ def _get_session_info(race_id: int) -> list[dict]:
     
     return sessions
 
-def _make_output_path(year, race_name, session_name) -> Path:
+def _make_output_path(year: int, race_name: str, session_name: str) -> Path:
+    """Internal tool to create output path to CSV results
+
+    Args:
+        year (_type_): The year of the F1 season
+        race_name (_type_): The name of the race
+        session_name (_type_): The name of the session
+
+    Returns:
+        Path: The path/to/the/results.csv
+    """
     session = "_".join(name for name in session_name.lower().split(" "))
     return PROCESSED_DATA_DIR / str(year) / race_name / f"{session}.csv"
 
-def run(start_year: int, end_year: int, progress: Progress):
+def _update_scrape_sessions_table(url: str) -> None:
+    """Updates the 'has_data' column in the 'scrape_sessions' table to signify that data has been collected
+
+    Args:
+        url (str): The URL of the session results page
+    """    
+    with connection.get_db(DB_PATH) as conn:  # type: ignore
+        cursor = conn.cursor()
+        cursor.execute("""UPDATE scrape_sessions
+                             SET has_data = 1 
+                           WHERE url = ?;
+                        """,(url,)
+                            )
+        output = cursor.fetchall()
+
+def run(start_year: int, end_year: int, progress: Progress) -> None:
     # setup progress bar
     num_seasons = end_year + 1 - start_year
-    year_task = progress.add_task(f"[bold magenta]Current year: {start_year}[/bold magenta]", total=num_seasons)
+    processing_task = progress.add_task(f"[bold magenta]Current year: {start_year}[/bold magenta]", total=num_seasons)
     tasks = []
 
     for year in range(start_year, end_year+1):
-        progress.update(year_task, description=f"[bold magenta]Current year: {year}[/bold magenta]")
+        progress.update(processing_task, description=f"[bold magenta]Current year: {year}[/bold magenta]")
 
         # get race_ids
         race_ids = database_query.get_race_ids(year=year, has_sessions=True)
-        race_task = progress.add_task(f"└ Getting sessions", total=len(race_ids))
-        tasks.append(race_task)
+        year_task = progress.add_task(f"└ Getting sessions", total=len(race_ids))
+        tasks.append(year_task)
 
         for race_id in race_ids:
             race_name = database_query.get_race_name(race_id)
-            progress.update(race_task, description=f"└ Getting sessions for [b]{race_name}[b]")
+            progress.update(year_task, description=f"└ Getting sessions for [b]{race_name}[b]")
             session_data = _get_session_info(race_id=race_id)
             
             for session in session_data:
-                progress.update(race_task, description=f"└ Processing results for [b]{race_name}:\n  - [dim]{session["session_name"]}[/dim][b]")
+                progress.update(year_task, description=f"└ Processing results for [b]{race_name}:\n  - [dim]{session["session_name"]}[/dim][b]")
                 session_html = fm.load_html_file(filepath=session["filepath"])
                 session_results = extract_results.get_results(soup=session_html)
+                
+                if not race_name: # so pylance won't have a fit
+                    continue 
                 
                 output_path = _make_output_path(year, race_name, session["session_name"])
                 fm.write_to_csv(data=session_results,
                                 csv_path=output_path,
                                 headers=fm.get_headers(session_results))
+                
                 results_cleaner.run(year,
                                     race_id,
                                     session_name=session["session_name"],
                                     results=session_results,
                                     url=session["url"])
-            progress.advance(race_task)
+                
+                _update_scrape_sessions_table(url=session["url"])
+
+            progress.advance(year_task)
         
         if year == end_year:
             pipe = "└"
@@ -89,14 +132,16 @@ def run(start_year: int, end_year: int, progress: Progress):
             pipe = "├"
 
         progress.update(year_task, description=f"{pipe} [green][b]{year}:[/b] All session results processed.[/green]")
-        progress.advance(year_task)
+        progress.advance(processing_task)
 
     # clean up tasks
     for task_id in tasks:
         progress.remove_task(task_id)
 
+    progress.update(processing_task, description=f"[green]✓ Data from sessions in {start_year} -> {end_year} extracted[/green]")
+
 def test():
     with get_progress_bar() as migration_progress:
-        run(1986,1986,migration_progress)
+        run(2000,2026,migration_progress)
 
 test()

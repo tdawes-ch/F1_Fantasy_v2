dfb6f1fff1336cbf63fa3a2bbaa969a3061d6b85
tdawes-ch <tomdawes3@gmail.com>
2026-06-30 18:35:39 +0100

    moved folder

--- testing/code/api_calls/openf1_test.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-# You pinpoint exactly the session and driver you want via URL queries
-url = "https://api.openf1.org/v1/car_data?driver_number=1&session_key=9161"
-response = requests.get(url).json()
-
-# You get a raw list of chronological data points
-for data_point in response[:50]:
-    print(f"Time: {data_point['date']} | Speed: {data_point['speed']} km/h")

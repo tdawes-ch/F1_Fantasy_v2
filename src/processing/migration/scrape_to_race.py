65661970c4ac23e322d02750281d5563b69ea93e
tdawes-ch <tomdawes3@gmail.com>
2026-07-03 20:01:49 +0100

    added to and from date columns added extra parameter (update) which determines whether data gets overwritten or not.

--- src/processing/migration/scrape_to_race.py
+++ src/processing/migration/scrape_to_race.py
@@ -8,13 +8,13 @@ from pathlib import Path
 # format is [schema_to_schema_table] e.g. races_to_scrape_sessions
 # scrape_race_weekends -> race_races
 
-def scrape_to_race_races(db_path: Path):
+def scrape_to_race_races(db_path: Path, update: bool):
     migration_data = {}
     # get data from scrape_race_weekends
     with connection.get_db(db_path) as conn:  # type: ignore
         cursor = conn.cursor()
         cursor.execute("""
-                        SELECT race_id, race_name, circuit, city, year, round
+                        SELECT race_id, race_name, circuit, city, year, round, from_date, to_date
                           FROM scrape_race_weekends;
                        """)
         results = cursor.fetchall()
@@ -26,23 +26,51 @@ def scrape_to_race_races(db_path: Path):
                 "circuit":row[2],
                 "city":row[3],
                 "season":row[4],
-                "round":row[5]
+                "round":row[5],
+                "from_date":row[6],
+                "to_date":row[6]
                 }
             # write data to race_races
-            cursor.execute("""
-                            INSERT INTO race_races (race_id, name, circuit, city, season, round)
-                            VALUES (?, ?, ?, ?, ?, ?)
-                            ON CONFLICT(race_id) DO NOTHING;
-                        """,
-                        (migration_data["race_id"],
-                         migration_data["name"],
-                         migration_data["circuit"],
-                         migration_data["city"],
-                         migration_data["season"],
-                         migration_data["round"])
-                        )
+            if update:
+                cursor.execute("""
+                                INSERT INTO race_races (race_id, name, circuit, city, season, round, from_date, to_date)
+                                VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+                                ON CONFLICT(race_id)
+                                DO UPDATE SET
+                                name = excluded.name,
+                                circuit = excluded.circuit,
+                                city = excluded.city,
+                                season = excluded.season,
+                                round = excluded.round,
+                                from_date = excluded.from_date,
+                                to_date= excluded.to_date;
+                            """,
+                            (migration_data["race_id"],
+                            migration_data["name"],
+                            migration_data["circuit"],
+                            migration_data["city"],
+                            migration_data["season"],
+                            migration_data["round"],
+                            migration_data["from_date"],
+                            migration_data["to_date"])
+                            )
+            else:
+                cursor.execute("""
+                                INSERT INTO race_races (race_id, name, circuit, city, season, round, from_date, to_date)
+                                VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+                                ON CONFLICT(race_id) DO NOTHING;
+                            """,
+                            (migration_data["race_id"],
+                            migration_data["name"],
+                            migration_data["circuit"],
+                            migration_data["city"],
+                            migration_data["season"],
+                            migration_data["round"],
+                            migration_data["from_date"],
+                            migration_data["to_date"])
+                            )
 
-def scrape_to_race_sessions(db_path: Path):
+def scrape_to_race_sessions(db_path: Path, update: bool):
     migration_data = {}
     # get data from scrape_race_weekends
     with connection.get_db(db_path) as conn:  # type: ignore
@@ -60,20 +88,36 @@ def scrape_to_race_sessions(db_path: Path):
                 "session_type":row[2]
                 # also need to get the date of the session from somewhere
                 }
-            # write data to race_races
-            cursor.execute(
-                        """
-                        INSERT INTO race_sessions (session_id, race_id, session_type)
-                        VALUES (?, ?, ?)
-                        ON CONFLICT(session_id) DO NOTHING;
-                        """,
-                        (migration_data["session_id"],
-                         migration_data["race_id"],
-                         migration_data["session_type"]
-                        )
-                        )
+            # write data to race_sessions
+            if update:
+                cursor.execute(
+                            """
+                            INSERT INTO race_sessions (session_id, race_id, session_type)
+                            VALUES (?, ?, ?)
+                            ON CONFLICT(session_id) 
+                            DO UPDATE SET
+                            session_type = excluded.session_type,
+                            race_id = excluded.race_id;
+                            """,
+                            (migration_data["session_id"],
+                            migration_data["race_id"],
+                            migration_data["session_type"]
+                            )
+                            )
+            else:
+                cursor.execute(
+                            """
+                            INSERT INTO race_sessions (session_id, race_id, session_type)
+                            VALUES (?, ?, ?)
+                            ON CONFLICT(session_id) DO NOTHING;
+                            """,
+                            (migration_data["session_id"],
+                            migration_data["race_id"],
+                            migration_data["session_type"]
+                            )
+                            )
             
-def scrape_to_race_seasons(db_path: Path):
+def scrape_to_race_seasons(db_path: Path, update: bool) -> None:
     migration_data = {}
     # get data from scrape_race_weekends
     with connection.get_db(db_path) as conn:  # type: ignore
@@ -90,35 +134,51 @@ def scrape_to_race_seasons(db_path: Path):
                 "expected_races":row[1],
                 "scraped_races":row[2]
                 }
-            # write data to race_races
-            cursor.execute("""
-                            INSERT INTO race_seasons (season, total_sessions, actual_sessions)
-                            VALUES (?, ?, ?)
-                            ON CONFLICT(season) DO NOTHING;
-                        """,
-                        (migration_data["year"],
-                         migration_data["expected_races"],
-                         migration_data["scraped_races"]
-                        )
-                        )
+            # write data to race_seasons
+            if update:
+                cursor.execute("""
+                               INSERT INTO race_seasons (season, total_sessions, actual_sessions)
+                               VALUES (?, ?, ?)
+                               ON CONFLICT(season)
+                               DO UPDATE SET
+                               total_sessions = excluded.total_sessions,
+                               actual_sessions = excluded.actual_sessions
+                               ;
+                               """,
+                               (migration_data["year"],
+                                migration_data["expected_races"],
+                                migration_data["scraped_races"]
+                               )
+                              )
+            else: 
+                cursor.execute("""
+                               INSERT INTO race_seasons (season, total_sessions, actual_sessions)
+                               VALUES (?, ?, ?)
+                               ON CONFLICT(season) DO NOTHING;
+                               """,
+                               (migration_data["year"],
+                                migration_data["expected_races"],
+                                migration_data["scraped_races"]
+                               )
+                              )
 
-def migrate_all(progress: Progress, db_path: Path = DB_PATH):
+def migrate_all(progress: Progress, db_path: Path = DB_PATH, update: bool = True):
     migration_task = progress.add_task(description=f"",total=3)
     # runs all migration sql scripts (this will run on setup)
 
     task_description = "Copying [blue]Seasion Data[/blue]..."
     progress.update(migration_task,description=f"{task_description:<30}")
-    scrape_to_race_seasons(db_path)
+    scrape_to_race_seasons(db_path, update)
     progress.advance(migration_task)
 
     task_description = "Copying [blue]Race Data[/blue]..."
     progress.update(migration_task,description=f"{task_description:<30}")
-    scrape_to_race_races(db_path)
+    scrape_to_race_races(db_path, update)
     progress.advance(migration_task)
 
     task_description = "Copying [blue]Session Data...[/blue]"
     progress.update(migration_task,description=f"{task_description:<30}")
-    scrape_to_race_sessions(db_path)
+    scrape_to_race_sessions(db_path, update)
     progress.advance(migration_task)
 
 
@@ -130,4 +190,9 @@ def do_migration():
     with get_progress_bar() as progress:
         migrate_all(progress, DB_PATH)
 
+def do_update():
+    with get_progress_bar() as progress:
+        migrate_all(progress, DB_PATH, update=True)
+
 #do_migration()
+#do_update()

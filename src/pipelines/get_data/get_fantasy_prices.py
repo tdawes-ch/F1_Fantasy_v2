272546f61c68261a4fc494babcf9f33a55224141
tdawes-ch <tomdawes3@gmail.com>
2026-07-04 22:46:53 +0100

    added bones to this file. Will get the most recent file, then the most recent race, and determine if we need to download the prices or not. If we don't need to download, the user is prompted whether to override this decision or not

--- src/pipelines/get_data/get_fantasy_prices.py
+++ src/pipelines/get_data/get_fantasy_prices.py
@@ -1,12 +1,43 @@
 # scrape page
 # extract data
 # write to db
+from scraping import fantasy_price_scraper
+from config.config import FANTASY_RAW_DIR, FANTASY_PROCESSED_DIR
+from toolbox import database_query
+from interface import prompts
+from pathlib import Path
 
 BASE_URL = r"https://fantasy.formula1.com/en/statistics/details"
 
-def run():
+def _do_we_need_to_scrape(recent_file: str, directory: Path, latest_race: dict):
+    # check if the most recent price information is after the most recent race.
+    # If yes, don't bother. If no, return true. If no file, return true also
+    file_date = f"{directory.name}-{recent_file.split(".")[0]}"
+    if file_date < latest_race["to_date"]:
+        return True
+    else:
+        return False
+    
+def _do_we_need_to_process():
+    # check if there are results in the database. If no, we need to process. If there are results, we check if they relate to the 
     pass
+        
+def run():
+    recent_file, directory = fantasy_price_scraper.get_latest_file(directory=FANTASY_RAW_DIR)
+    latest_race = database_query.get_recent_race(table_name="scrape_race_weekends", n_races_ago=0)
+    if not recent_file or not latest_race: # if there isn't a file, get it
+        fantasy_price_scraper.run(url=BASE_URL)
+    else:
+        if _do_we_need_to_scrape(recent_file, directory, latest_race):
+            fantasy_price_scraper.run(url=BASE_URL)
+        else: # do options
+            if prompts.scrape_anyway(message=f"{recent_file} already exists in '{directory}'"):
+                fantasy_price_scraper.run(url=BASE_URL)
+    recent_file, directory = fantasy_price_scraper.get_latest_file(directory=FANTASY_RAW_DIR)
+    if recent_file:
+        file_date = f"{directory.name}-{recent_file.split(".")[0]}"
 
 def test():
     run()
 
+test()

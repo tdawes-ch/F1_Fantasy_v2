7ac319f2e32e41254c28b08aebba64a00a812f58
tdawes-ch <tomdawes3@gmail.com>
2026-07-21 22:42:43 +0100

    temp file created for calculating fantasy points

---
+++ src/processing/calculation/fantasy_points_calculator.py
@@ -0,0 +1,7 @@
+"""
+Takes in a single race ID, goes through each session, calculates appropriate points for all drivers in each session
+"""
+from toolbox import database_query
+#
+def run(race_id: int):
+    pass
